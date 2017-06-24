78aef4e16307e18b477709953c05e360849b8c68
Hiroshige.Negishi <hirosige1@gmail.com>
2017-06-25 01:52:43 +0700

    fixed

--- spec/requests/apartment_options_spec.rb
+++ spec/requests/apartment_options_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "ApartmentOptions", type: :request do
   describe "GET /apartment_options" do
     it "works! (now write some real specs)" do
       get admin_apartments_apartment_options_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
 end
