7cca56546dc174e5e4cad22b45277db82fd71d18
Hiroshige.Negishi <hirosige1@gmail.com>
2017-06-25 13:12:22 +0700

    maintain migration files

--- db/migrate/20170611130101_create_apartment_infos.rb
+++ db/migrate/20170611130101_create_apartment_infos.rb
@@ -7,12 +7,12 @@ class CreateApartmentInfos < ActiveRecord::Migration
       t.decimal :longitude, :precision => 16, :scale => 13
       t.integer :floors
       t.string :made_of
-      t.string :reconstructed_or_not
-      t.string :renovated_or_not
-      t.string :car_park_or_not
-      t.string :bicycle_park_or_not
-      t.string :bike_park_or_not
-      t.string :can_have_pet_or_not
+      t.integer :reconstructed_or_not
+      t.integer :renovated_or_not
+      t.integer :car_park_or_not
+      t.integer :bicycle_park_or_not
+      t.integer :bike_park_or_not
+      t.integer :can_have_pet_or_not
       t.integer :contact_span
       t.string :conditions
       t.integer :update_fee
