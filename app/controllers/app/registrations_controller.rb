4a1836e4bb8fb062afbd53579bf10b565db6d70d
Hiroshige.Negishi <hirosige1@gmail.com>
2017-12-07 22:13:26 +0700

    change redirect url after sign up

--- app/controllers/app/registrations_controller.rb
+++ app/controllers/app/registrations_controller.rb
@@ -47,14 +47,16 @@ before_filter :configure_account_update_params, only: [:update]
 
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    super(resource)
+    new_user_session_path
+  end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    super(resource)
+    new_user_session_path
+  end
 
   protected
   def configure_sign_up_params
