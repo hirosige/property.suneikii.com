ae19153fe460f47827e7672b43f8d77496ab2157
Ton <nathakorn.tonch@gmail.com>
2018-01-07 18:50:08 +0700

    update gem and modify home-capybara chan

--- spec/features/top/home_spec.rb
+++ spec/features/top/home_spec.rb
@@ -11,22 +11,22 @@ describe "home", :type => :feature do
     it "has text" do
       expect(page).to have_text(Settings.site[:name])
       expect(page).to have_text('Select a kind of property')
-      expect(page).to have_text('© 2017 Property Suneikii Powered by Suneikii.com All Rights Reserved.')
+      expect(page).to have_text('© 2018 Property Suneikii Powered by Suneikii.com All Rights Reserved.')
 
       # Buttons
       expect(page).to have_content('Apartments')
       expect(page).to have_content('Condominiums')
-      expect(page).to have_content('Houses')
+      # expect(page).to have_content('Houses')
       expect(page).to have_content('Lands')
 
-      page.save_screenshot("#{ENV['TEST_IMG_PATH']}/top/home/home_en.png", full: true)
+      # page.save_screenshot("#{ENV['TEST_IMG_PATH']}/top/home/home_en.png", full: true)
     end
 
     it "click_link_apartments" do
       page.click_link('Apartments')
       expect(page).to have_current_path(apartment_province_list_path(:locale => :en))
 
-      page.save_screenshot("#{ENV['TEST_IMG_PATH']}/top/home/home_moved_to_apartments_en.png", full: true)
+      # page.save_screenshot("#{ENV['TEST_IMG_PATH']}/top/home/home_moved_to_apartments_en.png", full: true)
     end
   end
 
@@ -38,22 +38,22 @@ describe "home", :type => :feature do
     it "has text" do
       expect(page).to have_text(Settings.site[:name])
       expect(page).to have_text('物件を探す')
-      expect(page).to have_text('© 2017 Property Suneikii Powered by Suneikii.com All Rights Reserved.')
+      expect(page).to have_text('© 2018 Property Suneikii Powered by Suneikii.com All Rights Reserved.')
 
       # Buttons
       expect(page).to have_content('アパート')
       expect(page).to have_content('コンド')
-      expect(page).to have_content('家')
+      # expect(page).to have_content('家')
       expect(page).to have_content('土地')
 
-      page.save_screenshot("#{ENV['TEST_IMG_PATH']}/top/home/home_ja.png", full: true)
+      # page.save_screenshot("#{ENV['TEST_IMG_PATH']}/top/home/home_ja.png", full: true)
     end
 
     it "click_link_apartments" do
       page.click_link('アパート')
       expect(page).to have_current_path(apartment_province_list_path(:locale => :ja))
 
-      page.save_screenshot("#{ENV['TEST_IMG_PATH']}/top/home/home_moved_to_apartments_ja.png", full: true)
+      # page.save_screenshot("#{ENV['TEST_IMG_PATH']}/top/home/home_moved_to_apartments_ja.png", full: true)
     end
   end
 
@@ -65,11 +65,11 @@ describe "home", :type => :feature do
     it "has text" do
       expect(page).to have_text(Settings.site[:name])
       # TODO 物件を探す
-      expect(page).to have_text('© 2017 Property Suneikii Powered by Suneikii.com All Rights Reserved.')
+      expect(page).to have_text('© 2018 Property Suneikii Powered by Suneikii.com All Rights Reserved.')
 
       # Buttons
 
-      page.save_screenshot("#{ENV['TEST_IMG_PATH']}/top/home/home_th.png", full: true)
+      # page.save_screenshot("#{ENV['TEST_IMG_PATH']}/top/home/home_th.png", full: true)
     end
   end
 
